a6ee47ea18c2a9583fd24260cd64f21ab5ce6d74
Tom Scott <tubbo@psychedeli.ca>
2012-10-15 23:21:10 -0400

    Add Capistrano task support

---
+++ lib/rain/capistrano.rb
@@ -0,0 +1,21 @@
+# = Capistrano tasks
+#
+# A simple addon to Capistrano that defines some callbacks
+# and deploy tasks.
+#
+# The to_latest_tag task abstracts setting the branch of each
+# deployment to the current ReleaseTag.
+
+require 'capistrano'
+require 'rain/git_tools'
+
+module Rain
+  Capistrano::Configuration.instance(:must_exist).load do
+    after "to_stage", "to_latest_tag"
+    after "to_production", "to_latest_tag"
+
+    task :to_latest_tag do
+      set :branch, GitTools::ReleaseTag.current(rails_env)
+    end
+  end
+end
