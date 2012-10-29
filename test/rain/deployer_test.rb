911169d877101b510004cae542c016270a3ec102
Tom Scott <tubbo@psychedeli.ca>
2012-10-28 22:35:20 -0400

    Oh yeah

--- test/rain/deployer_test.rb
+++ test/rain/deployer_test.rb
@@ -4,8 +4,13 @@ class Rain::DeployerTest < ActiveSupport::TestCase
   describe "DeployerTest: bare invocation" do
     setup { @command = %x(./bin/rain) }
 
-    should "deploy to production" do
+    should "deploy a new tag to stage" do
+      skip "suck it"
       assert_match 'Got a handful of stacks better grab an umbrella', @command
+    end
+
+    should "deploy the same tag that's on stage to production" do
+      assert_match 'jflksjflksjfl;', @command
     end
   end
 
