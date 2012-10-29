7517d7674098ff9544c46c0e717f7dd87a6abb3c
Tom Scott <tubbo@psychedeli.ca>
2012-10-28 22:39:50 -0400

    Add tests for on stage and on prod

--- test/rain/deployer_test.rb
+++ test/rain/deployer_test.rb
@@ -2,20 +2,33 @@ require 'test_helper'
 
 class Rain::DeployerTest < ActiveSupport::TestCase
   describe "DeployerTest: bare invocation" do
-    setup { @command = %x(./bin/rain) }
+    before { @command ||= %x(./bin/rain) }
 
-    should "deploy a new tag to stage" do
-      skip "suck it"
+    should "deploy to production" do
       assert_match 'Got a handful of stacks better grab an umbrella', @command
     end
+  end
 
-    should "deploy the same tag that's on stage to production" do
-      assert_match 'jflksjflksjfl;', @command
+  describe "DeployerTest: specific environment invocation" do
+    context "on stage" do
+      before { @command ||= %x(./bin/rain on stage) }
+
+      should "deploy a new tag to stage" do
+        assert_match 'Deploying existing tag', @command
+      end
+    end
+
+    context "on production" do
+      before { @command ||= %x(./bin/rain on production) }
+
+      should "deploy the same tag that's on stage to production" do
+        assert_match 'Deploying existing tag', @command
+      end
     end
   end
 
   describe "DeployerTest: help invocation for 'on'" do
-    before { @command = %x(./bin/rain help on) }
+    before { @command ||= %x(./bin/rain help on) }
 
     should "prompt for an environment" do
       assert_match 'rain on ENVIRONMENT', @command
