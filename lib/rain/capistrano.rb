e92847eca4a034715a13ab69738ac05ad9043d83
Tom Scott <tubbo@psychedeli.ca>
2012-10-31 12:58:01 -0400

    Remove deploy email entirely

--- lib/rain/capistrano.rb
+++ lib/rain/capistrano.rb
@@ -23,44 +23,5 @@ module Rain
         set :branch, ReleaseTag.latest
       end
     end
-
-    after "deploy:update",  "deploy:email"
-
-    namespace :deploy do
-      desc "Send deployment notification email to the development team, set by the `:developers` setting in your config/deploy.rb."
-      task :email, :role => :app do
-        unless developers.blank?
-          versions = YAML.load_file(Pathname(__FILE__).dirname.expand_path.join('versions.yml'))
-
-          body = if !versions.nil? and versions[rails_env]
-            git_tags = %x(git tag).split("\n").
-              select { |l| l =~ /^rel_/ }.
-              map { |l| ReleaseTag.new(l) }.sort
-            current_release = versions[rails_env]
-            previous_release = git_tags[-2]
-            change_log = %x(git log --pretty=format:'%cn (%cr): %s%n%mhttps://github.com/eLocal/elocal_web/commit/%H %n' --date=relative #{previous_release}..#{current_release})
-
-            "<img src=\"#{deploy_success_image}\">
-  <p>#{deployer} has updated the #{rails_env.downcase} API to #{versions[rails_env]}.</p>
-  <p>
-  Change log: https://github.com/eLocal/affiliates/compare/#{previous_release}...#{current_release}
-  </p>
-  <pre>
-  #{change_log}
-  </pre>
-  <p>
-    Love,<br>
-    #{deployer}
-  </p>"
-          else
-            "API Environment #{rails_env} has been updated to master."
-          end
-
-          subject = "API #{rails_env} environment bumped to #{versions[rails_env]}"
-
-          %x(echo 'To: #{developers}\nSubject: #{subject}\nContent-Type: text/html;charset="us-ascii"\n\n<html>#{body}</html>' | sendmail -t) rescue puts "Console mail could not send message"
-        end
-      end
-    end
   end
 end
