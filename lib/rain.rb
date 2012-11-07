8e2c018296af275a6630be89ebc60d81a14b475e
Tom Scott <tubbo@psychedeli.ca>
2012-11-07 10:46:54 -0500

    Don't require rails and upgrade to latest Ruby

--- lib/rain.rb
+++ lib/rain.rb
@@ -5,7 +5,6 @@ require 'rain/semantic_version'
 require 'rain/config'
 require 'rain/git_tools'
 require 'rain/deployer'
-require 'rails/all'
 
 require 'pathname'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path("../../Gemfile", Pathname.new(__FILE__).realpath)
